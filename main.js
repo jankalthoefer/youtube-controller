2cfd80af20c9dab5922f88a9dd961c1ee7d32f6d
jankalthoefer <jan@kalthoefer.me>
2024-12-15 22:34:51 +0100

    fix double playing playslist

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-const playlists = ['PLQZnrgDn-_7R9eCb279SwMASxxxE3ONxd', 'PLNRuQxam6R9z4lIIo1oVDv4v1XWQkFc1V', 'PLy2urwVIMDlmuIJwinFabCb1r5kCr9Kn3'];
+const playlists = [
+    'PLDFFGwD6I7tL5eLlXB_YKYl2BwmL6UKBk', //Calmm trains
+    'PLDFFGwD6I7tIiuvyLH4sImVkJ4oRL6Wux', // trains US
+    'PLDFFGwD6I7tJ8scE6VFhDAmijPH6uWcbF', // shorts
+];
 // 2. This code loads the IFrame Player API code asynchronously.
 const defaultVideo = 'IsubCyjw8ic';
 
@@ -11,11 +15,19 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 // switchPlaylist(0);
 
 function switchPlaylist(index) {
+    console.log('Switch to playlist', index);
     const playlistId = playlists[index];
-    console.log(playlistId);
-    console.log(player);
 
-    player.loadPlaylist({ list: playlistId, listType: 'playlist' });
+    if (player.getPlaylist() && player.getPlaylistId() === playlistId) {
+        return; // Prevent reloading the same playlist
+    }
+    console.log('before', player.getPlaylistId());
+    player.stopVideo();
+    player.loadPlaylist({ list: playlistId, listType: 'playlist', index: 0, suggestedQuality: 'hd720' });
+    setTimeout(() => {
+        player.playVideo();
+        console.log('after', player.getPlaylistId());
+    }, 1000);
 }
 
 // 4. The API will call this function when the video player is ready.
@@ -24,26 +36,6 @@ function onPlayerReady(event) {
     event.target.playVideo();
 }
 
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
-var done = false;
-function onPlayerStateChange(event) {
-    if (event.data != YT.PlayerState.PLAYING || event.data != YT.PlayerState.BUFFERING) {
-        console.log('play');
-
-        //<!-- event.target.playVideo(); -->
-    }
-    //console.log('🚀 ~ event:', event);
-    //console.log('🚀 ~ YT.PlayerState:', YT.PlayerState);
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-        // setTimeout(stopVideo, 6000);
-        done = true;
-    }
-}
-function stopVideo() {
-    player.stopVideo();
-}
 function skipVideo() {
     player.nextVideo();
 }
@@ -58,7 +50,29 @@ function onYouTubeIframeAPIReady(playlistId) {
         videoId: defaultVideo,
         events: {
             onReady: onPlayerReady,
-            onStateChange: onPlayerStateChange,
+            // onStateChange: onPlayerStateChange,
         },
     });
 }
+
+// // 5. The API calls this function when the player's state changes.
+// //    The function indicates that when playing a video (state=1),
+// //    the player should play for six seconds and then stop.
+// var done = false;
+// function onPlayerStateChange(event) {
+//     if (event.data != YT.PlayerState.PLAYING || event.data != YT.PlayerState.BUFFERING) {
+//         console.log('play');
+
+//         //<!-- event.target.playVideo(); -->
+//     }
+//     //console.log('🚀 ~ event:', event);
+//     //console.log('🚀 ~ YT.PlayerState:', YT.PlayerState);
+//     if (event.data == YT.PlayerState.PLAYING && !done) {
+//         // setTimeout(stopVideo, 6000);
+//         done = true;
+//     }
+// }
+
+// function stopVideo() {
+//     player.stopVideo();
+// }
